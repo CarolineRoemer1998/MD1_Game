4bbff3219799fd24e85391c1b60bc09a592ee35f
xxPigelxx <129988942+xxPigelxx@users.noreply.github.com>
2025-06-03 18:22:41 +0200

    Level2 und nextlevel funktion

--- Scripts/scene_switcher.gd
+++ Scripts/scene_switcher.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var current_scene = null
+var current_level = 1
+var levelCount = 2
 func _ready() -> void:
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
@@ -14,3 +16,11 @@ func _deferred_switch_scene(res_path):
 	current_scene = new_scene.instantiate()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
+
+func go_to_next_level():
+	var nextLevel = current_level + 1
+	if nextLevel <= levelCount:
+		switch_scene("res://Scenes/Level"+ str(nextLevel) +".tscn")
+		current_level = nextLevel
+	else:
+		switch_scene("res://Scenes/Menu/MainMenu.tscn")
