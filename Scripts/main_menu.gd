3895faba6a82ce12fc11edce4b23b2db34fcc8fd
cora_lyn <caroroemer@gmx.de>
2025-06-03 14:43:18 +0200

    Player can merge 2 creatures to 1
    When player possesses a creature and moves it to another creature, if the creatures "match", the creatures merge into a new one.

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	SceneSwitcher.switch_scene("res://Scenes/game.tscn")
+	SceneSwitcher.switch_scene("res://Scenes/Level1.tscn")
 
 
 func _on_stettings_pressed() -> void:
