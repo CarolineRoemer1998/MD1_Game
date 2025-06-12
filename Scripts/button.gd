32c1c6a7e8b82d3296eb1144ad2d388b388e12a3
cora_lyn <caroroemer@gmx.de>
2025-06-12 12:39:07 +0200

    Added Button Sounds

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -8,6 +8,8 @@ var active: bool = false
 @onready var button_green: Sprite2D = $Button_GREEN
 @onready var button_red: Sprite2D = $Button_RED
 @onready var area: Area2D = $Area2D
+@onready var audio_push_button: AudioStreamPlayer2D = $AudioPushButton
+@onready var audio_leave: AudioStreamPlayer2D = $AudioLeave
 
 func _ready() -> void:
 	area.body_entered.connect(_on_body_entered)
@@ -20,6 +22,7 @@ func _on_body_entered(body: Node) -> void:
 		_update_button_color()
 		print(active)
 		emit_signal("activated")
+		audio_push_button.play()
 
 func _on_body_exited(body: Node) -> void:
 	# Optional: only deactivate if no bodies are left
@@ -29,6 +32,7 @@ func _on_body_exited(body: Node) -> void:
 		_update_button_color()
 		print(active)
 		emit_signal("deactivated")
+		audio_leave.play()
 
 func _update_button_color() -> void:
 	button_green.visible = active
