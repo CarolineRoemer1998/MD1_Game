15d06a96195bd6b5ed8e6f27ecef82ecea974c80
xxPigelxx <129988942+xxPigelxx@users.noreply.github.com>
2025-07-01 16:55:39 +0200

    added a fade in after going to a new level

--- Scripts/scene_switcher.gd
+++ Scripts/scene_switcher.gd
@@ -1,5 +1,9 @@
 extends Node
 
+@onready var fade_scene = preload("res://Scenes/Menu/fade.tscn")
+var fade_node = null
+var fade_animation = null
+
 var current_scene = null
 var current_level = 0
 var levelCount = 4
@@ -10,33 +14,44 @@ func _ready() -> void:
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 	
+	
 func switch_scene(res_path, pause_current=false):
 	call_deferred("_deferred_switch_scene", res_path, pause_current)
 
 func _deferred_switch_scene(res_path, pause_current=false):
+	# Handle pause or regular cleanup
 	if pause_current:
-		# Instead of freeing current scene, just hide it and store reference
 		current_scene.hide()
 		paused_scene = current_scene
 		is_paused = true
 		get_tree().paused = true
 	else:
-		# If we're returning from a paused scene, free the paused scene
 		if is_paused:
 			paused_scene.free()
 			is_paused = false
 			get_tree().paused = false
-		# Otherwise free current scene normally
 		elif current_scene:
 			current_scene.free()
-	
-	var new_scene = load(res_path)
-	current_scene = new_scene.instantiate()
+
+	# Load and instantiate the new scene first
+	var new_scene = load(res_path).instantiate()
+	current_scene = new_scene
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
 
+	# Inject fade if it's a level scene
+	if res_path.contains("/Scenes/Level"):
+		if not fade_node:
+			fade_node = fade_scene.instantiate()
+		current_scene.add_child(fade_node)  # Important: add it to new scene, not root
+		fade_animation = fade_node.get_node("AnimationPlayer")
+		fade_animation.play("fade")
+		await fade_animation.animation_finished
+
+
 func go_to_next_level():
 	if current_level == levelCount:
+		current_level = 0
 		switch_scene("res://Scenes/Menu/credit_scene.tscn")
 		return
 	var nextLevel = current_level + 1
