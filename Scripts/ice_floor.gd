9fa3dfe07495d044e13dde348cfc37ff513a39e4
xxPigelxx <129988942+xxPigelxx@users.noreply.github.com>
2025-07-08 21:25:47 +0200

    Changed IceFloor Satruration and added corners to the level

--- Scripts/ice_floor.gd
+++ Scripts/ice_floor.gd
@@ -1,3 +1,9 @@
 extends Node2D
 
 class_name IceFloor
+@export var texture: Texture2D = preload("res://Sprites/IceFloor/Ice-floor.png")
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	sprite_2d.texture = texture
