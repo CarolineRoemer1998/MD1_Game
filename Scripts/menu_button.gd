554384803871f9fbc9a85d47ba8e7d607fca1f2a
xxPigelxx <129988942+xxPigelxx@users.noreply.github.com>
2025-07-09 12:15:57 +0200

    MouseHoversound

--- Scripts/menu_button.gd
+++ Scripts/menu_button.gd
@@ -1,6 +1,6 @@
 extends Button
 
-const HOVER_SOUND = "res://Sounds/step4.mp3"
+const HOVER_SOUND = "res://Sounds/step5.mp3"
 const PRESS_SOUND = "res://Sounds/Push Button 4.mp3"
 
 func _ready():
@@ -8,10 +8,11 @@ func _ready():
 	await get_tree().process_frame
 
 	focus_entered.connect(self._on_focus_entered)
+	mouse_entered.connect(self._on_focus_entered)
 	pressed.connect(self._on_button_pressed)
 	
 func _on_focus_entered():
-	AudioManager.play_sfx(HOVER_SOUND)
-		
+	AudioManager.play_sfx(HOVER_SOUND)	
+	
 func _on_button_pressed():
 	AudioManager.play_sfx(PRESS_SOUND)
